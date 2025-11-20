518776e09c5afff2ae2efc8b2ee79326b0725ec4
root <root@ip-172-31-24-182.ec2.internal>
2025-11-20 03:24:26 +0000

    code is added

---
+++ app.py
@@ -0,0 +1,8 @@
+x = 10
+y = 5
+print(f"Addition: {x + y}")
+print(f"Subtraction: {x - y}")
+print(f"Multiplication: {x * y}")
+print(f"Division: {x / y}")
+print(f"Floor Division: {x // y}") # Rounds down to the nearest whole number
+print(f"Modulo (Remainder): {x % y}")
